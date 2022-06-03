9c32950ce7e42563a49cc597367ffc3ac834429e
doublebyte1 <info@doublebyte.net>
2022-06-03 12:18:06 +0200

    - Hardcoded masked dataset

--- pygeoapi/es-entrypoint.sh
+++ pygeoapi/es-entrypoint.sh
@@ -32,13 +32,13 @@
 
 set +e
 
-dataset="${DATASET:-obs}"
+# dataset="${DATASET:-obs}"
 
-echo  "Update dataset name to $dataset"
+# echo  "Update dataset name to $dataset"
 
 # Workaround to avoid resource busy
-cp /pygeoapi/local.config-orig.yml /pygeoapi/local.config.yml
-sed -i "s/DATASET/$dataset/g" /pygeoapi/local.config.yml
+# cp /pygeoapi/local.config-orig.yml /pygeoapi/local.config.yml
+# sed -i "s/DATASET/$dataset/g" /pygeoapi/local.config.yml
 
 echo  "Install Curl"
 

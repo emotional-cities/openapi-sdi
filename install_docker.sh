815ba7ccf7cdf0c7597bb4d697a9ac8db0bd7868
doublebyte <doublebyte@hushmail.com>
2026-07-03 07:48:14 +0000

    - bumped docker compose version on install script

--- install_docker.sh
+++ install_docker.sh
@@ -18,7 +18,7 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/5.3.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
